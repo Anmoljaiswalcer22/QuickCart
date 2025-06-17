995a79aacb328cb6dc4615b81e5ffcc2bfdbb6a9
anmoljaiswal291 <anmoljaiswal291@gmail.com>
2025-06-17 20:23:03 +0530

    jjij

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import dbConnect from '@/config/db';
-import User from '../models/user.js';
+import User from "@/models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quick-cart" });
